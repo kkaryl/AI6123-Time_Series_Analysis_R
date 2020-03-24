ea755aefe00adced6b022a85b0e8da3030b34a2d
karyl <jhong0819@gmail.com>
2020-03-24 22:57:57 +0800

    working v3

--- 20200323_Assignment2_DrugV3.R
+++ 20200323_Assignment2_DrugV3.R
@@ -6,7 +6,7 @@
 # 
 # It is due on 3 April.
 
-# library(dplyr)
+library(dplyr)
 library(lubridate) # for converting data to year month day
 # library(caTools)
 library(astsa)
@@ -19,16 +19,20 @@ df_data = read.delim("./data/drug.txt", header=TRUE, sep = ",")
 head(df_data)
 
 global.freq <<- 12
-
 df_data <- df_data %>% mutate(date = ymd(date))
+global.start <<- ymd(as.Date(df_data$date[[1]]))
+global.end <<- ymd(as.Date(df_data$date[[nrow(df_data)]]))
+
 plot(df_data,
      main="Monthly anti-diabetic drug sales",
      xlab="Year", ylab="Drug Sales")
 lines(df_data$date, df_data$value,type="l")
 
 # Convert to time series data
-ts_all <- ts(df_data$value, start=c(1991, 7), end=c(2008, 6), frequency=global.freq)
-acf2(ts_all)
+ts_all <- ts(df_data$value, start=c(year(global.start), month(global.start)),
+             end=c(year(global.end), month(global.end)), frequency=global.freq)
+# ts_all <- ts(df_data$value, start=c(1991, 7), end=c(2008, 6), frequency=global.freq)
+# acf2(ts_all)
 
 # Stationary test
 # adf.test(ts_all)
@@ -72,13 +76,13 @@ plotforecast <- function(ts_all, ...) {
   }
   ggplot(pltdata, aes(date, value, colour = model)) +
     geom_line()+
-    geom_vline(aes(xintercept = as.numeric(date[length(ts_all)])), 
+    geom_vline(aes(xintercept = as.numeric(date[length(ts_all)])),
                linetype = "longdash", color = "black")+
     ylab("")+
     xlab("")+
     ggtitle("Monthly anti-diabetic drug sales")+
-    theme(axis.line = element_line(), axis.text=element_text(color='black'), 
-          axis.title = element_text(colour = 'black'), legend.text=element_text(), 
+    theme(axis.line = element_line(), axis.text=element_text(color='black'),
+          axis.title = element_text(colour = 'black'), legend.text=element_text(),
           legend.title=element_text(), legend.key = element_rect(colour = "black"))
 }
 
@@ -87,13 +91,13 @@ plot.hw <- plotforecast(ts_all, pred.hw.add, "HW ADD", pred.hw.mul, "HW MUL")
 grid.arrange(plot.hw, ncol = 1)
 
 ### Predict using Seasonal ARIMA Model
-acf2(ts_all)
+# acf2(ts_all)
 ts_all_d1 = diff(ts_all, differences = 1)
 plotforecast(ts_all_d1)
-acf2(ts_all_d1)
+# acf2(ts_all_d1)
 ts_all_d2 = diff(ts_all, differences = 2)
 plotforecast(ts_all_d2)
-acf2(ts_all_d2)
+# acf2(ts_all_d2)
 
 fit.auto.arima <- auto.arima(ts_train, max.p=2, max.q=2,
                              max.P=2, max.Q=2, max.d=2, max.D=2, allowdrift = TRUE,
